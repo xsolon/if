20cfe5f48f1bcb490a841fe94f963376beb5efb2
xsolon <martin@xsolon.net>
2017-05-27 20:52:03 -0400

    Tracing source test project

---
+++ xSolon.Tracing.Source/content/Diagnostics/ExtendedTraceSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace xSolon.Tracing
+{
+    public class ExtendedTraceSource : TraceSource
+    {
+        public void Indent()
+        {
+
+            foreach (TraceListener listener in this.Listeners)
+            {
+                listener.IndentLevel++;
+            }
+        }
+
+        public void UnIndent()
+        {
+
+            foreach (TraceListener listener in this.Listeners)
+            {
+                listener.IndentLevel--;
+            }
+
+        }
+
+        #region ctor
+        public ExtendedTraceSource(string sourceName) : base(sourceName)
+        {
+
+        }
+
+        public ExtendedTraceSource() : base("xSolon")
+        {
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ xSolon.Tracing.Source/content/Diagnostics/TracedProxy.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
+using System.Text;
+
+namespace xSolon.Tracing
+{
+    public class TracedProxy<T> : RealProxy where T : TracedClass
+    {
+
+        private readonly T _dal;
+
+        public TracedProxy(T dal)
+            : base(dal.GetType())
+        {
+            _dal = dal;
+
+            typeName = _dal.GetType().Name;
+
+        }
+
+        private string typeName = "";
+
+        private Dictionary<string, object> GetParams(IMethodCallMessage methodCall)
+        {
+            var res = new Dictionary<string, object>();
+
+            for (var i = 0; i < methodCall.InArgCount; i++)
+            {
+                var key = methodCall.GetInArgName(i);
+                var val = methodCall.GetInArg(i);
+
+                var stringVal = GetSerializedObject(val);
+
+                res.Add(key, stringVal);
+            }
+
+            return res;
+        }
+
+        private static string GetSerializedObject(object obj)
+        {
+
+            var res = string.Empty;
+
+            if (obj == null)
+            {
+                res = "NULL";
+            }
+            else
+            {
+                try
+                {
+                    //res = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                }
+                catch (Exception ex)
+                {
+                    res = ex.ToString();
+                }
+            }
+
+            return res;
+
+        }
+
+        public delegate void Action();
+
+        TimeSpan Elapsed(Action action)
+        {
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+            action();
+
+            stopWatch.Stop();
+
+            return stopWatch.Elapsed;
+
+        }
+
+        void TraceParameters(Dictionary<string, object> callParams)
+        {
+
+            _dal.Indent();
+
+            try
+            {
+
+                foreach (var kv in callParams)
+                {
+                    var msg = string.Format("{0}: {1}", kv.Key, kv.Value);
+
+                    _dal.TraceData(TraceEventType.Verbose, 1602, msg);
+                }
+            }
+            finally
+            {
+                _dal.UnIndent();
+
+            }
+
+        }
+
+        public override IMessage Invoke(IMessage msg)
+        {
+            var methodCall = msg as IMethodCallMessage;
+            var methodInfo = methodCall.MethodBase as MethodInfo;
+            try
+            {
+                object result = null;
+
+                var elapsed = Elapsed(() =>
+                {
+                    result = methodInfo.Invoke(_dal, methodCall.InArgs);
+                });
+
+                if ("TraceInformation|TraceData|TraceEvent|get_Trace|Indent|UnIndent".Contains(methodCall.MethodName))
+                {
+
+                }
+                else
+                {
+                    var inParams = GetParams(methodCall);
+
+                    inParams.Add("Result", GetSerializedObject(result));
+
+                    _dal.TraceEvent(TraceEventType.Verbose, 1601, "{1,-10} - {0,-10}: {2}", methodCall.MethodName, typeName, elapsed);
+
+                    TraceParameters(inParams);
+                }
+
+                return new ReturnMessage(result, null, 0, methodCall.LogicalCallContext, methodCall);
+            }
+            catch (Exception e)
+            {
+                _dal.TraceEvent(TraceEventType.Error, 1603, "{1} - Exception {0} executing '{1}'", e, methodCall.MethodName, typeName);
+
+                _dal.TraceData(TraceEventType.Error, 1604, e.ToString());
+
+                throw;
+            }
+        }
+
+        public new T GetTransparentProxy()
+        {
+
+            return base.GetTransparentProxy() as T;
+        }
+
+    }
+
+}
